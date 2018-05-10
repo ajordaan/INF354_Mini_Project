879e5db41ed55e5d29da5bb3354b1170ce75068b
DESKTOP-UL6AVSI\Daddy_COol <ruanckruger@gmail.com>
2018-05-10 10:06:03 +0200

    Add Login

--- INF354_MiniProj/Controllers/HomeController.cs
+++ INF354_MiniProj/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using INF354_MiniProj.Models;
 
 namespace INF354_MiniProj.Controllers
 {
@@ -13,11 +15,20 @@ namespace INF354_MiniProj.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Login(string searchterm = null)
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            var _db = new furnatureEntities();
+
+            IQueryable<customer> custQuery = 
+                from cust in _db.customers
+                where cust.email == searchterm
+                select cust;
+
+            return View(custQuery);
+            
+           
         }
 
         public ActionResult Contact()
--- INF354_MiniProj/Models/Model1.Designer.cs
+++ INF354_MiniProj/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Daddy_COol\source\repos\INF354_MiniProj\INF354_MiniProj\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\inf354ass\MiniProj\INF354_Mini_Project\INF354_MiniProj\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
